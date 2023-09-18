6af2ca1712912dcb442d75c55a3ffa38053fcdf3
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-09-18 18:52:44 +0530

    added dummy test

---
+++ test/audio_controller_test.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_test/flutter_test.dart';
+// import 'package:audio_player/controller/audio_controller.dart';
+
+void main() {
+  group('AudioController', () {
+//     late AudioController audioController;
+
+    test('audioTest', () {
+      expect(1, 1);
+    });
+
+//     setUp(() {
+//       audioController = AudioController();
+//     });
+  });
+}
+
+//     test('play should start playing audio', () async {
+//       const link = 'audio.mp3';
+//       await audioController.play(link);
+//       expect(audioController.audioPlayer.playing, true);
+//     });
+
+//     test('pause should pause audio', () async {
+//       const link = 'audio.mp3';
+//       await audioController.play(link);
+//       await audioController.pause();
+//       expect(audioController.audioPlayer.playing, false);
+//     });
+
+//     test('stop should stop audio', () async {
+//       const link = 'audio.mp3';
+//       await audioController.play(link);
+//       await audioController.stop();
+//       expect(audioController.audioPlayer.playing, false);
+//     });
+
+//     test('seek should seek to beginning of audio', () async {
+//       const link = 'audio.mp3';
+//       await audioController.play(link);
+//       await audioController.seek();
+//       expect(audioController.audioPlayer.position, Duration.zero);
+//     });
+
+//     tearDown(() {
+//       audioController.dispose();
+//     });
+//   });
+// }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  group('demo test', () {
+    test('demo test', () {
+      expect(1, 1);
+    });
+  });
+}
